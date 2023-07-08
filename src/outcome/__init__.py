93495d6510a6388fa03a0686c70343c78104a340
Spencer Brown <spencerb21@live.com>
2023-07-08 13:24:45 +1000

    WWhen importing public symbols mark as exported

--- src/outcome/__init__.py
+++ src/outcome/__init__.py
@@ -1,7 +1,13 @@
 """Top-level package for outcome."""
 
-from ._impl import Error, Outcome, Value, acapture, capture
-from ._util import AlreadyUsedError, fixup_module_metadata
+from ._impl import (
+    Error as Error,
+    Outcome as Outcome,
+    Value as Value,
+    acapture as acapture,
+    capture as capture,
+)
+from ._util import AlreadyUsedError as AlreadyUsedError, fixup_module_metadata
 from ._version import __version__
 
 __all__ = (
