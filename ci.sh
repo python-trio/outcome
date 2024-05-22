1ce2b921e866f7f79df8a3af85b3c69a20900994
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-05-21 20:38:59 -0500

    Update continuous integration script to be similar to Trio

--- ci.sh
+++ ci.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-set -ex
+set -ex -o pipefail
 
-CHECK_FILES="setup.py src tests"
+CHECK_FILES="src tests"
 YAPF_VERSION=0.20.1
 
-python -m pip install -U pip setuptools wheel
+# Log some general info about the environment
+echo "::group::Environment"
+uname -a
+env | sort
+echo "::endgroup::"
 
-python setup.py sdist --formats=zip
-pip install dist/*.zip
+################################################################
+# We have a Python environment!
+################################################################
 
+echo "::group::Versions"
+python -c "import sys, struct, ssl; print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('openssl:', ssl.OPENSSL_VERSION, ssl.OPENSSL_VERSION_INFO)"
+echo "::endgroup::"
+
+echo "::group::Install dependencies"
+python -m pip install -U pip build
+python -m pip --version
+
+python -m build
+python -m pip install dist/*.whl
+echo "::endgroup::"
+
+echo "::group::Setup for tests"
 # Install dependencies.
 pip install -Ur test-requirements.txt
+echo "::endgroup::"
 
 if [ "$CHECK_FORMATTING" = "1" ]; then
+    echo "::group::Yapf"
     pip install yapf==${YAPF_VERSION} "isort>=5" mypy pyright
     if ! yapf -rpd $CHECK_FILES; then
+        echo "::endgroup::"
         cat <<EOF
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -30,10 +51,15 @@ in your local checkout.
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 EOF
+        echo "::error:: yapf found issues"
         exit 1
+    else
+        echo "::endgroup::"
     fi
 
+    echo "::group::isort"
     if ! isort --check-only --diff $CHECK_FILES ; then
+        echo "::endgroup::"
         cat <<EOF
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -48,10 +74,15 @@ in your local checkout.
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 EOF
+        echo "::error:: isort found issues"
         exit 1
+    else
+        echo "::endgroup::"
     fi
 
+    echo "::group::Mypy"
     if ! mypy src/ tests/type_tests.py ; then
+        echo "::endgroup::"
         cat <<EOF
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -66,9 +97,13 @@ in your local checkout.
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 EOF
+        echo "::error:: Mypy found issues"
         exit 1
+    else
+        echo "::endgroup::"
     fi
 
+    echo "::group::Pyright"
     if ! pyright --verifytypes outcome src/outcome/ ; then
         cat <<EOF
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -84,12 +119,17 @@ in your local checkout.
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 EOF
+        echo "::error:: Pyright found issues"
         exit 1
+    else
+        echo "::endgroup::"
     fi
 
     exit 0
 fi
 
+echo "::group:: Run Tests"
 pytest -W error -ra -v tests --cov --cov-config=.coveragerc
+echo "::endgroup::"
 
 bash <(curl -s https://codecov.io/bash)
