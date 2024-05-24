0b5d1c3f67b7a55f45d96590f52e0c32ca2bacbd
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-05-24 13:27:18 -0500

    Don't print SSL info and fix comment

--- ci.sh
+++ ci.sh
@@ -16,7 +16,7 @@ echo "::endgroup::"
 ################################################################
 
 echo "::group::Versions"
-python -c "import sys, struct, ssl; print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('openssl:', ssl.OPENSSL_VERSION, ssl.OPENSSL_VERSION_INFO)"
+python -c "import sys, struct; print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8)"
 echo "::endgroup::"
 
 echo "::group::Install dependencies"
