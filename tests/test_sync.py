a28ec1aaf57c4faa5df5a862bd89da60a78a8d37
Thomas Grainger <tagrain@gmail.com>
2026-01-29 12:14:38 +0000

    Revert "make error and value private fields accessed by properties"
    This reverts commit 2606efb73f3d2044ed47c00ce62755c43696d998.

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -10,7 +10,6 @@ from outcome import AlreadyUsedError, Error, Value
 def test_Outcome():
     v = Value(1)
     assert v.value == 1
-    v = Value(1)
     assert repr(v) == "Value(1)"
     assert v.unwrap() == 1
     assert repr(v) == "Value(<AlreadyUsed>)"
@@ -22,16 +21,13 @@ def test_Outcome():
 
     exc = RuntimeError("oops")
     e = Error(exc)
-    error = e.error
-    assert error is exc
-    e = Error(exc)
+    assert e.error is exc
     assert repr(e) == f"Error({exc!r})"
-    e = Error(exc)
     with pytest.raises(RuntimeError):
         e.unwrap()
     with pytest.raises(AlreadyUsedError):
         e.unwrap()
-    assert repr(e) == "Error(<AlreadyUsed>)"
+    assert repr(e) == "Error(<DEAD>)"
 
     e = Error(exc)
     with pytest.raises(TypeError):
@@ -105,9 +101,8 @@ def test_capture():
 
     e = outcome.capture(raise_ValueError, "two")
     assert type(e) == Error
-    error = e.error
-    assert type(error) is ValueError
-    assert error.args == ("two",)
+    assert type(e.error) is ValueError
+    assert e.error.args == ("two",)
 
 
 def test_inheritance():
